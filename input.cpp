24b2473eec5bc5485b270bd72b9178476d722f8b
shahid <shahid@localhost.localdomain>
2021-02-23 22:30:59 +0530

    not working

---
+++ input.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "s.h"
+
+//This is a comment
+/*This is also a comment */
+
+void main() {
+
+	cout << "hi";
+	int a = 10,b = 20;
+	int c,d;
+	int e;
+	int f = 1;
+	++a;
+	while(a != f && a){
+		a--;
+		if(a==2){
+			while(1);
+		}
+		else{
+			while(a==1);
+		}
+	}
+}
